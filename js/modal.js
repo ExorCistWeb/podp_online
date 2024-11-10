b8aa753affe972e13ccd962503533aac077d6604
MirMurMay <vladimirovich.aleksei.ru@gmail.com>
2024-11-10 09:06:18 +0700

    lk_add_d

--- js/modal.js
+++ js/modal.js
@@ -18,3 +18,22 @@ $(document).ready(function() {
         }
     });
 });
+
+// Показать окно при нажатии на кнопку "Найти"
+document.getElementById('findButton').addEventListener('click', function(event) {
+    event.preventDefault(); // предотвращаем отправку формы
+    document.getElementById('popup').style.display = 'flex';
+});
+
+// Закрытие окна при нажатии на крестик
+document.querySelector('.close_popup_time_error').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('popup').style.display = 'none';
+});
+
+// Закрытие окна при нажатии на фон
+document.getElementById('popup').addEventListener('click', function(event) {
+    if (event.target === this) {
+        this.style.display = 'none';
+    }
+});
